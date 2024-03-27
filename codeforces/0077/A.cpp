4cb5a83fff2d1b4bcbc5e565437f6570837dc2b8
arjundevensharma <129678257+arjundevensharma@users.noreply.github.com>
2024-03-27 14:56:02 -0400

    Initial commit.

---
+++ codeforces/0077/A.cpp
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		String n = in.next();
+		
+		if (n.contains("0000000") || n.contains("1111111"))
+			System.out.println("YES");
+		else
+			System.out.println("NO");
+	}
+
+}
