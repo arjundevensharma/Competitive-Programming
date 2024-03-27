719e5edb75feab5cdcbfcec3e7b1b56468675f74
arjundevensharma <129678257+arjundevensharma@users.noreply.github.com>
2024-03-26 20:43:12 -0400

    Initial commit.

---
+++ CSES/SortingAndSearching/DistinctNumbers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define sz(a) (int)a.size()
+ 
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define FORR(i, a, b) for(int i = (a); i >= (b); i--)
+ 
+//directions of movement
+int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
+int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
+int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
+ 
+int n;
+ 
+void solve() {
+    map<int, bool> m;
+    int q, v;
+    cin >> q;
+    FR(i, q) {
+        cin >> v;
+        m[v] = true;
+    }
+    cout << m.size() << "\n";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    solve();
+}
