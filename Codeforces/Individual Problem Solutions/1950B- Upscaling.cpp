c199cbe3130a3c409ae5f6507d68f6461fcafa92
Arjun Sharma <129678257+arjundevensharma@users.noreply.github.com>
2024-03-29 11:33:12 -0400

    Create 1950B- Upscaling.cpp

---
+++ Codeforces/Individual Problem Solutions/1950B- Upscaling.cpp
@@ -0,0 +1,51 @@
+/**
+ * Solution by arjundevensharma
+ * Date: 29/03/2024
+ * Contest: Codeforces Round 937 (Div. 4)
+ * Problem: B. Upscaling
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define sz(a) (int)a.size()
+
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define FORR(i, a, b) for(int i = (a); i >= (b); i--)
+
+int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
+int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
+int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
+
+int n;
+
+void solve() {
+    cin >> n;
+
+    FR (i, n) {
+        FR (j, n) {
+            if ( (j + i) % 2 == 0) cout << "##";
+            else cout << "..";
+        }
+        cout << "\n";
+        FR (j, n) {
+            if ( (j + i) % 2 == 0) cout << "##";
+            else cout << "..";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
