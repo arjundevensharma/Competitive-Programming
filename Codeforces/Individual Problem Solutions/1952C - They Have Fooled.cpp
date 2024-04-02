c04e3362ed374b9f98b0e6d5885551acacc1df72
Arjun Sharma <129678257+arjundevensharma@users.noreply.github.com>
2024-04-02 15:14:04 -0400

    Create 1952C - They Have Fooled.cpp

---
+++ Codeforces/Individual Problem Solutions/1952C - They Have Fooled.cpp
@@ -0,0 +1,39 @@
+/**
+ * Solution by arjundevensharma
+ * Date: 02/01/2024
+ * Contest: April Fools Day Contest 2024
+ * Problem: C. They Have Fooled
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define sz(a) (int)a.size()
+
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define FORR(i, a, b) for(int i = (a); i >= (b); i--)
+
+int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
+int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
+int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
+
+int n;
+
+void solve() {
+    cin >> n;
+
+    int answers[] = {10, 10, 8, 9, 8, 7, 7, 7, 7, 0, 9, 6, 8};
+    cout << answers[n];
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+        solve();
+}
