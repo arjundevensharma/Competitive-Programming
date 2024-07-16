f0e21aa60cedddf1fa2287b0889a91d8c03f1d59
Arjun Sharma <129678257+arjundevensharma@users.noreply.github.com>
2024-07-16 09:15:34 -0400

    Update DistinctNumbers.cpp

--- CSES/SortingAndSearching/DistinctNumbers.cpp
+++ CSES/SortingAndSearching/DistinctNumbers.cpp
@@ -1,36 +1,48 @@
+/**
+ * Solution by arjundevensharma
+ * Date: July 16, 2024
+ * Contest: CSES Problem Set
+ * Problem: Distinct Numbers
+**/
+
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define ll long long
 #define all(v) v.begin(), v.end()
 #define pb push_back
 #define sz(a) (int)a.size()
- 
+
 #define FR(i, n) for(int i = 0; i < (n); i++)
 #define FOR(i, a, b) for(int i = (a); i < (b); i++)
 #define FORR(i, a, b) for(int i = (a); i >= (b); i--)
- 
-//directions of movement
+
 int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
 int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
 int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
- 
+
 int n;
- 
+
 void solve() {
-    map<int, bool> m;
-    int q, v;
-    cin >> q;
-    FR(i, q) {
-        cin >> v;
-        m[v] = true;
+    cin >> n;
+
+    set <int> a;
+
+    FR(i,n) {
+        int b;
+        cin >> b;
+        a.insert(b);
     }
-    cout << m.size() << "\n";
+
+    cout << a.size();
 }
- 
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
- 
-    solve();
+
+    //int t;
+    //cin >> t;
+    //while (t--)
+        solve();
 }
