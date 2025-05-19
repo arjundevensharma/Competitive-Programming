39f0fe85118a418e4cffa9d68118df4be14d2aae
Arjun Sharma <129678257+arjundevensharma@users.noreply.github.com>
2025-05-19 18:56:12 -0400

    Update WeirdAlgorithm.cpp

--- CSES/IntroductoryProblems/WeirdAlgorithm.cpp
+++ CSES/IntroductoryProblems/WeirdAlgorithm.cpp
@@ -4,31 +4,40 @@ using namespace std;
 #define ll long long
 #define all(v) v.begin(), v.end()
 #define pb push_back
+#define mp make_pair
 #define sz(a) (int)a.size()
-#define ar array
+ 
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define FORR(i, a, b) for(int i = (a); i >= (b); i--)
+ 
+int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
+int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
+int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
  
 ll n;
  
 void solve () {
     cin >> n;
  
-    cout << n << ' ';
+    cout << n << " ";
  
-    while (n > 1) {
-        if (n % 2 == 0) {
-            n/=2;
-            cout << n << ' ';
-        } else {
-            n*= 3;
-            n++;
-            cout << n << " ";
-        }
+    while (n != 1) {
+        if (n % 2 == 0) n /= 2;
+        else n = (n * 3) + 1;
+        cout << n << " ";
     }
 }
  
 int main () {
-    ios::sync_with_stdio();
+    ios::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
  
-    solve();
+    //int t; 
+    //cin >> t;
+ 
+    //while (t--) {
+        solve();
+    //}
 }
