86ad1b8b72a85f78c803cff0690c76d560254d1a
Arjun Sharma <129678257+arjundevensharma@users.noreply.github.com>
2025-05-19 18:54:31 -0400

    Update IncreasingArray.cpp

--- CSES/IntroductoryProblems/IncreasingArray.cpp
+++ CSES/IntroductoryProblems/IncreasingArray.cpp
@@ -4,42 +4,50 @@ using namespace std;
 #define ll long long
 #define all(v) v.begin(), v.end()
 #define pb push_back
+#define mp make_pair
 #define sz(a) (int)a.size()
  
 #define FR(i, n) for(int i = 0; i < (n); i++)
 #define FOR(i, a, b) for(int i = (a); i < (b); i++)
 #define FORR(i, a, b) for(int i = (a); i >= (b); i--)
  
-//directions of movement
 int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
 int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
 int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
  
-long n;
+int n;
  
 void solve () {
     cin >> n;
  
-    long a[n];
+    vector<int>b;
  
-    FR(i, n) cin >> a[i];
+    ll prev = -1;
+    ll sum = 0;
  
-    long moves=0;
+    FR(i, n) {
+        ll a;
+        cin >> a;
  
-    FOR(i, 1, n) {
-        if (a[i] < a[i - 1]) {
-            long tot = (a[i-1] - a[i]);
-            a[i] = a[i - 1];
-            moves += tot;
+        if (a < prev) {
+            sum += prev - a;
+        } else {
+            prev = a;
         }
     }
- 
-    cout << moves;
+    
+    cout << sum;
 }
  
 int main () {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
  
-    solve();
+    //int t; 
+    //cin >> t;
+ 
+    //while (t--) {
+        solve();
+    //}
 }
