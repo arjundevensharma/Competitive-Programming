1529ee9a507dee82f0b31e6929af53abf67c8a03
Arjun Sharma <129678257+arjundevensharma@users.noreply.github.com>
2025-05-19 18:55:45 -0400

    Update MissingNumber.cpp

--- CSES/IntroductoryProblems/MissingNumber.cpp
+++ CSES/IntroductoryProblems/MissingNumber.cpp
@@ -4,29 +4,25 @@ using namespace std;
 #define ll long long
 #define all(v) v.begin(), v.end()
 #define pb push_back
+#define mp make_pair
 #define sz(a) (int)a.size()
  
 #define FR(i, n) for(int i = 0; i < (n); i++)
 #define FOR(i, a, b) for(int i = (a); i < (b); i++)
 #define FORR(i, a, b) for(int i = (a); i >= (b); i--)
  
-//directions of movement
 int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
 int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
 int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
  
-int n;
+ll n;
  
 void solve () {
     cin >> n;
-    int sum = 0;
-    int origN = n;
-    n++;
-    while (n--) {
-        sum += n;
-    }
  
-    FR (i, origN - 1) {
+    ll sum = (n + 1) * (n/2.0);
+ 
+    FR(i,n - 1) {
         int a;
         cin >> a;
         sum -= a;
@@ -38,6 +34,12 @@ void solve () {
 int main () {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
  
-    solve();
+    //int t; 
+    //cin >> t;
+ 
+    //while (t--) {
+        solve();
+    //}
 }
