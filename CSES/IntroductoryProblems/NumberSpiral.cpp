6d9700ee5ec45afa0471e171420a3fee5cc8b8cc
Arjun Sharma <129678257+arjundevensharma@users.noreply.github.com>
2025-05-26 20:01:03 -0400

    Update NumberSpiral.cpp

--- CSES/IntroductoryProblems/NumberSpiral.cpp
+++ CSES/IntroductoryProblems/NumberSpiral.cpp
@@ -4,13 +4,13 @@ using namespace std;
 #define ll long long
 #define all(v) v.begin(), v.end()
 #define pb push_back
+#define mp make_pair
 #define sz(a) (int)a.size()
  
-#define FR(i, n) for(ll i = 0; i < (n); i++)
-#define FOR(i, a, b) for(ll i = (a); i < (b); i++)
-#define FORR(i, a, b) for(ll i = (a); i >= (b); i--)
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define FORR(i, a, b) for(int i = (a); i >= (b); i--)
  
-//directions of movement
 int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
 int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
 int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
@@ -20,27 +20,33 @@ ll y, x;
 void solve () {
     cin >> y >> x;
  
-    ll upper = max(y, x);
+    ll out;
  
-    ll base = upper * upper;
- 
-    if (base % 2 == 0) {
-        base -= abs(1 - x);
-        base -= abs(upper - y);
+    if (y >= x) {
+        if (y % 2 != 0) {
+            out = ( (y-1) * (y-1)) + x;
+        } else {
+            out = (y*y) - (x-1);
+        }
     } else {
-        base -= abs(1 - y);
-        base -= abs(upper - x);
+        if (x % 2 == 0) {
+            out = ((x-1) * (x-1)) + y;
+        } else {
+            out = (x*x) - (y-1);
+        }
     }
- 
-    cout << base << "\n";
+    cout << out << "\n";
 }
  
 int main () {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int t;
-    cin>> t;
+    cout.tie(0);
  
-    while (t--)
+    int t; 
+    cin >> t;
+ 
+    while (t--) {
         solve();
+    }
 }
