f2cb5030f37d94ef62b73bc7df9a3cb6de4c1a59
Arjun Sharma <129678257+arjundevensharma@users.noreply.github.com>
2025-05-26 20:55:38 -0400

    Update TrailingZeros.cpp

--- CSES/IntroductoryProblems/TrailingZeros.cpp
+++ CSES/IntroductoryProblems/TrailingZeros.cpp
@@ -4,36 +4,42 @@ using namespace std;
 #define ll long long
 #define all(v) v.begin(), v.end()
 #define pb push_back
+#define mp make_pair
 #define sz(a) (int)a.size()
  
-#define FR(i, n) for(ll i = 0; i < (n); i++)
-#define FOR(i, a, b) for(ll i = (a); i < (b); i++)
-#define FORR(i, a, b) for(ll i = (a); i >= (b); i--)
+#define FR(i, n) for(int i = 0; i < (n); i++)
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define FORR(i, a, b) for(int i = (a); i >= (b); i--)
  
-//directions of movement
 int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
 int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
 int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
- 
-ll n;
- 
+
+int n;
+
 void solve () {
     cin >> n;
-    
-    ll tot = 0;
- 
-    while (n > 0) {
-        n /= 5;
-        tot += n;
+
+    int t = 0;
+    int d = 5;
+
+    while (d <= n) {
+        t += (n / d);
+        d *= 5;
     }
- 
-    cout << tot;
+
+    cout << t;
 }
  
 int main () {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
- 
-    solve();
+
+    //int t; 
+    //cin >> t;
+
+    //while (t--) {
+        solve();
+    //}
 }
