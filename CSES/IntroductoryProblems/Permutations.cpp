6e4aab003a4750d0d94e184bbca33db575ec2f51
Arjun Sharma <129678257+arjundevensharma@users.noreply.github.com>
2025-05-26 20:00:00 -0400

    Update Permutations.cpp

--- CSES/IntroductoryProblems/Permutations.cpp
+++ CSES/IntroductoryProblems/Permutations.cpp
@@ -4,13 +4,13 @@ using namespace std;
 #define ll long long
 #define all(v) v.begin(), v.end()
 #define pb push_back
+#define mp make_pair
 #define sz(a) (int)a.size()
  
 #define FR(i, n) for(int i = 0; i < (n); i++)
 #define FOR(i, a, b) for(int i = (a); i < (b); i++)
 #define FORR(i, a, b) for(int i = (a); i >= (b); i--)
  
-//directions of movement
 int dx[] = {1, -1, 0, 0, 0, 0, 0, 0, 1, -1};
 int dy[] = {0, 0, 1, -1, 0, 0, 1, -1, 0, 0};
 int dz[] = {0, 0, 0, 0, 1, -1, 0, 0, 0, 0};
@@ -20,19 +20,15 @@ int n;
 void solve () {
     cin >> n;
  
-    if (n > 1 && n < 4) cout << "NO SOLUTION";
-    else if (n == 1) cout << 1;
-    else if (n == 4) cout << "2 4 1 3";
+    if (n <= 3 && n >= 2) cout << "NO SOLUTION";
     else {
-        int a = n;
-        while (a > 0) {
-            cout << a << " ";
-            a -= 2;
-        }
-        a = n - 1;
-        while (a > 0) {
-            cout << a << " ";
-            a -= 2;
+        FR(i, n) {
+            int c = 2 + (i * 2);
+            if (c > n) {
+                c -= n;
+                if (c % 2 == 0) c --;
+            }
+            cout << c << " ";
         }
     }
 }
@@ -40,6 +36,12 @@ void solve () {
 int main () {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
  
-    solve();
+    //int t; 
+    //cin >> t;
+ 
+    //while (t--) {
+        solve();
+    //}
 }
